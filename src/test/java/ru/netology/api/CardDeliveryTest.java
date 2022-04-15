eaa501a23f2d549ee684ea2fcc8f12c90e47aac9
Alexander Syayalev <alexxs@inbox.ru>
2022-04-16 00:25:41 +0300

    all right but not all V2

--- src/test/java/ru/netology/api/CardDeliveryTest.java
+++ src/test/java/ru/netology/api/CardDeliveryTest.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
@@ -22,20 +23,14 @@ public class CardDeliveryTest {
     @Test
     public void ShouldSubmitRequest() {
         open("http://localhost:9999/");
-
-        //SelenideElement form = $("[form form_size_m form_theme_alfa-on-white]");
         SelenideElement form = $("form[enctype='application/x-www-form-urlencoded']");
         $("input[placeholder='Город']").setValue("Архангельск");
-        $("input[type='tel']").setValue("17.04.2022");
-        //$(byText("Дата встречи")).setValue("10.05.2022");
+        $("input.input__control[placeholder='Дата встречи']").setValue("19.04.2022");
         $("input[name='name']").setValue("Валенков Алеша");
         $("input[name='phone']").setValue("+79008006600");
         $("label[data-test-id='agreement']").click();
         $(byText("Забронировать")).click();
-
         Configuration.timeout = 15000;
         $(withText("Успешно!")).shouldBe(visible);
-        //$(withText("Встреча успешно забронирована на ")).shouldBe(visible);
-
     }
 }
