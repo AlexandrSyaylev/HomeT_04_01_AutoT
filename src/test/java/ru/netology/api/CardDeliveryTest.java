68332f382b91e5488a6023c5647929d702d77263
Alexander Syayalev <alexxs@inbox.ru>
2022-04-17 23:24:42 +0300

    add date method, change withText to shouldHave & add second task

--- src/test/java/ru/netology/api/CardDeliveryTest.java
+++ src/test/java/ru/netology/api/CardDeliveryTest.java
@@ -1,5 +1,6 @@
 package ru.netology.api;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,8 @@ import org.openqa.selenium.support.ui.Select;
 
 import java.text.*;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -24,16 +27,15 @@ import static java.awt.SystemColor.menu;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class CardDeliveryTest {
-    GregorianCalendar date = new GregorianCalendar();
-    DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
 
     @Test
     public void ShouldSubmitRequest() {
-        date.roll(Calendar.DATE, 3);
-        String dateMeetin = dateFormat.format(date.getTime());
-        System.out.println(dateMeetin);
+        String dateMeetin = generateDate(3);
         open("http://localhost:9999/");
-        SelenideElement form = $("form[enctype='application/x-www-form-urlencoded']");
         $("input[placeholder='Город']").setValue("Архангельск");
         $("input.input__control[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME));
         $("input.input__control[placeholder='Дата встречи']").sendKeys(Keys.BACK_SPACE);
@@ -42,10 +44,29 @@ public class CardDeliveryTest {
         $("input[name='phone']").setValue("+79008006600");
         $("label[data-test-id='agreement']").click();
         $(byText("Забронировать")).click();
-        Configuration.timeout = 15000;
-        $(withText("Успешно!")).shouldBe(visible);
-        $(byText("Встреча успешно забронирована на")).shouldBe(visible);
-        $(byText(dateMeetin)).shouldBe(visible);
+        $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + dateMeetin));
+    }
+
+
+    @Test
+    public void SecondTask() {
+        String dateMeetin = generateDate(7);
+        System.out.println(dateMeetin);
+        open("http://localhost:9999/");
+        $("input[placeholder='Город']").click();
+        $("input[placeholder='Город']").sendKeys("а", "р");
+        $("input[placeholder='Город']").sendKeys(Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.ENTER);
+        $("button.icon-button").click();
+        $("input.input__control[placeholder='Дата встречи']").
+                sendKeys(Keys.ARROW_DOWN, Keys.ARROW_RIGHT, Keys.ARROW_RIGHT, Keys.ARROW_RIGHT, Keys.ARROW_RIGHT, Keys.ENTER);
+        $("input[name='name']").setValue("Валенков Алеша");
+        $("input[name='phone']").setValue("+79008006600");
+        $("label[data-test-id='agreement']").click();
+        $(byText("Забронировать")).click();
+        $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + dateMeetin));
+
     }
 
 }
